eca97f59a3d318c035f17394eaabb04f8613cb1d
Rokibul Alom <itismehey975@gmail.com>
2024-09-18 23:53:20 +0600

    second commit

---
+++ js files/login.js
@@ -0,0 +1,29 @@
+// step of this jsfile
+/****
+ * 1. set buttuon with eventlistener
+ * 2.collect the data or value from input field
+ * 3. to check the phone number and pin
+ * 4.to go home page
+ */
+
+// step 1
+document.getElementById('btn-click').addEventListener('click',function(event){
+    event.preventDefault();
+    //step one done
+
+    // step two
+    const collectnumber = document.getElementById('number-input').value;
+    const collectpin = document.getElementById('pin-input').value;
+    // step two done
+
+    // step three
+    if(collectnumber === '01' && collectpin === '1234'){
+        console.log('yes this is right');
+        window.location.href ='/homepage.html';
+    }
+    else(alert('number or pin is wrong'));
+    // step three done 
+
+    // step four
+    
+})
